05e31a04135fdb639644cd3ad6984083696b3e14
noboru-i <ishikura.noboru@gmail.com>
2015-06-23 16:40:18 +0900

    install deploy gate

--- app/src/main/java/hm/orz/chaos114/oauthsamples/App.java
+++ app/src/main/java/hm/orz/chaos114/oauthsamples/App.java
@@ -2,6 +2,7 @@ package hm.orz.chaos114.oauthsamples;
 
 import android.app.Application;
 
+import com.deploygate.sdk.DeployGate;
 import com.facebook.FacebookSdk;
 
 /**
@@ -11,6 +12,9 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // DeployGate
+        DeployGate.install(this);
 
         // facebook
         FacebookSdk.sdkInitialize(getApplicationContext());
